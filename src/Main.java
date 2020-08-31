bf149bf5182f4e278b88bfaf2dffdb4801743c31
Andrey Motorygin <motoryginad@gmail.com>
2020-08-31 18:00:04 +0300

    ForgotCode

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int transfer = 500000000;
+        int total = balance + transfer;
+            System.out.println(total);
+    }
+
+}
