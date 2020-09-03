c646bc95e0970ad13ff080e3b02bb4e9957c702b
Andrey Motorygin <motoryginad@gmail.com>
2020-09-03 11:09:33 +0300

    DeletedMain.jv

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int balance = 2000000000;
-        int transfer = 500000000;
-        int total = balance + transfer;
-            System.out.println(total);
-    }
-
-}
